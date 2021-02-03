658a01d3d379a1db931abc0c79660d1fc3eeed95
AcMitra <78456416+AcMitra@users.noreply.github.com>
2021-02-03 09:47:39 +0800

    Create index.php
    Part One

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" conent="width=device-width", initial-scale=1.0'>
+  <title>Document</title>
+  </head>
+<body>
+  <p>Welcome to my page [Write you Fullname]</p>
+</body>
+</html>
